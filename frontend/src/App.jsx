43d2d1b23139747a0eade9179c68896ed76791e7
daolam1734 <daolam1734@gmail.com>
2025-07-25 08:36:50 +0700

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,21 @@ import axios from 'axios';
 import Login from './features/auth/Login';
 import Register from './features/auth/Register';
 import Home from './pages/Home';
+import About from './pages/About';
 import Header from './components/shared/Header';
 import Footer from './components/shared/Footer';
-
+import Contact from './pages/Contact';
 import AdminDashboard from './features/admin/AdminDashboard';
 import LibrarianDashboard from './features/librarian/LibrarianDashboard';
 import BookManager from './features/librarian/books/BookManager';
 import ReaderManager from './features/librarian/readers/ReaderManager';
-import BorrowManager from './features/librarian/borrowed/BorrowManager';
-
-import BorrowedBooks from './features/borrowed/BorrowedBooks';
+import BorrowManager from './features/librarian/borrow/BorrowManager';
+import BorrowedBooks from './features/reader/BorrowedBooks';
 import Profile from './features/profile/Profile';
 
 import ProtectedRoute from './components/routes/ProtectedRoute';
 import ProtectedRouteAdmin from './components/routes/ProtectedRouteAdmin';
 import ProtectedRouteLibrarian from './components/routes/ProtectedRouteLibrarian';
-
-// ✅ Giao diện danh mục sách chung cho mọi người
-import BookCatalog from './components/shared/book/BookCatalog';
 
 import { Toaster } from 'react-hot-toast';
 
@@ -43,8 +40,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/catalog" element={<BookCatalog />} /> {/* ✅ Danh mục sách chung */}
-
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         {/* Người dùng */}
         <Route
           path="/profile"
@@ -54,15 +51,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route
-          path="/borrowed"
-          element={
-            <ProtectedRoute>
-              <BorrowedBooks />
-            </ProtectedRoute>
-          }
-        />
-
         {/* Admin */}
         <Route
           path="/admin"
@@ -106,6 +94,7 @@ function App() {
             </ProtectedRouteLibrarian>
           }
         />
+        <Route path="/borrowed" element={<BorrowedBooks />} />
       </Routes>
       <Footer />
       <Toaster position="top-right" reverseOrder={false} />
